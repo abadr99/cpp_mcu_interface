6ebfcbce7bf215b71b1fcf14887e4c8e98f96e4c
Mariamahmed44 <madiam.ahmed300@gmail.com>
2023-12-21 01:45:19 +0200

    adding HW test for gpio

--- tests/hardware/gpio/basic_test_2.cpp
+++ tests/hardware/gpio/basic_test_2.cpp
@@ -13,14 +13,19 @@ int main() {
     Gpio<Port::kPortA> gpioA;
      
     
-    // 2] Set pin A1 as output
+    // 2] Set pin A1 as input pullup
     gpioA.SetDirection<Pin::kPin1, DirectionState::kInputPullUp>();
+    // 3] Set pin A0 as output
     gpioA.SetDirection<Pin::kPin0, DirectionState::kOutput>();
+    // 4]create a value & initialize it to khigh
+    // 5] It will be assigned to the return value of read method
     DigitalLevel val=kHigh;
     while (1) {
-       
+    // 6]read value of A1
     val= gpioA.Read<Pin::kPin1>();
+    // 7]for debouncing 
         _delay_ms(50);
+   // 8]if the value is low , turn on the led , else turn it off, for A0
         if ( val == kLow)
         {
         gpioA.Write<Pin::kPin0, DigitalLevel::kHigh>();
