2f981f7e0fdd28feddfc5f45f7d95de00b8d1db7
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2024-01-19 22:58:29 +0200

    Testing new modification in register class

--- tests/integration/utils/main.cpp
+++ tests/integration/utils/main.cpp
@@ -8,14 +8,14 @@ int main () {
         avr::test::AVRTest TEST("Extract-Bits");
         const uint32_t VALUE = 0xAABBCCDD;
         uint8_t RESULT = utils::ExtractBits<uint32_t, 0, 3>(VALUE); //0b1101
-        TEST.Expected_Eq(static_cast<uint8_t>(0b1101), RESULT);
+        TEST.Expect_Eq(static_cast<uint8_t>(0b1101), RESULT);
         RESULT = utils::ExtractBits<uint32_t, 3, 5>(VALUE); //0b11
-        TEST.Expected_Eq(static_cast<uint8_t>(0b11), RESULT);
+        TEST.Expect_Eq(static_cast<uint8_t>(0b11), RESULT);
         RESULT = utils::ExtractBits<uint32_t, 13, 20>(VALUE); //0b11011110
-        TEST.Expected_Eq(static_cast<uint8_t>(0b11011110), RESULT);
+        TEST.Expect_Eq(static_cast<uint8_t>(0b11011110), RESULT);
         RESULT = utils::ExtractBits<uint32_t, 28, 31>(VALUE); //0b1010
-        TEST.Expected_Eq(static_cast<uint8_t>(0b1010), RESULT);
+        TEST.Expect_Eq(static_cast<uint8_t>(0b1010), RESULT);
         RESULT = utils::ExtractBits<uint32_t, 0>(VALUE); //0b1
-        TEST.Expected_Eq(static_cast<uint8_t>(0b1), RESULT);
+        TEST.Expect_Eq(static_cast<uint8_t>(0b1), RESULT);
     }
 }
