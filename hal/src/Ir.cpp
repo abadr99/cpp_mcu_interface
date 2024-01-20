d3e0d4c58edb0346b8cacce81e94b2c1bb4b1b97
AbdelrahmanBadr22 <badr@Ubuntu.myguest.virtualbox.org>
2024-01-20 19:10:11 +0200

    Refactir IR interface

--- hal/src/Ir.cpp
+++ hal/src/Ir.cpp
@@ -10,32 +10,13 @@ using namespace avr::mcal::gpio;
 using namespace avr::hal;
 using namespace avr::hal::ir;
 
-template <avr::types::Port TPort, avr::types::Pin TPin>
-Ir<TPort, TPin>::Ir() 
-: gpio::InputDeviceInterface<TPort>(TPin),
-  IrPin_(TPin)
+Ir::Ir(Port port, Pin pin) : gpio::InputDeviceInterface(port), 
+                             IrPin_(pin)
 {/* EMPTY */}
 
-template <avr::types::Port TPort, avr::types::Pin TPin>
-IrState Ir<TPort, TPin>::GetState() {
+typename
+Ir::IRState Ir::GetState() {
     using DL = DigitalLevel;
-    return GetPinState<TPin>() == DL::kHigh ? IrState::kDetected 
-                                            : IrState::kNotDetected;
+    return GetPinState(IrPin_) == DL::kHigh ? IRState::kDetected 
+                                            : IRState::kNotDetected;
 }
-
-// --- Template instantiation 
-#define X(_pin)     template class Ir<Port::kPortA, Pin::k##_pin>;
-    ATMEGA32_PINS
-#undef X
-
-#define X(_pin)     template class Ir<Port::kPortB, Pin::k##_pin>;
-    ATMEGA32_PINS
-#undef X
-
-#define X(_pin)     template class Ir<Port::kPortC, Pin::k##_pin>;
-    ATMEGA32_PINS
-#undef X
-
-#define X(_pin)     template class Ir<Port::kPortD, Pin::k##_pin>;
-    ATMEGA32_PINS
-#undef X
