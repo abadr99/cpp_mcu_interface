dccf9317ee084a429f38ff81761bd7a8f162f206
Mariamahmed44 <115891752+Mariamahmed44@users.noreply.github.com>
2024-02-19 17:51:24 +0200

    Update Keypad.cpp

--- hal/src/Keypad.cpp
+++ hal/src/Keypad.cpp
@@ -12,30 +12,42 @@ using namespace avr::hal;
 using namespace avr::hal::keypad;
 using OP = avr::hal::gpio::OutputDeviceInterface;
 using IP = avr::hal::gpio::InputDeviceInterface;
-Keypad::Keypad(Port port,
-               Pin c1, Pin c2, Pin c3, Pin c4, 
-               Pin r1, Pin r2, Pin r3, Pin r4):
-               KeypadPort_(port),
-               pins_{c1, c2, c3, c4, r1, r2, r3, r4}
+Keypad::Keypad(Port colport, 
+               Port rowport, 
+               uint8_t colsize, 
+               uint8_t rowsize, 
+               Pin* colpins, 
+               Pin* rowpins):
+               ColPort_(colport), 
+               RowPort_(rowport), 
+               ColSize_(colsize), 
+               RowSize_(rowsize), 
+               ColPins_(colpins), 
+               RowPins_(rowpins) 
+
 { //set column pins to be output & row pins to be input
- for (uint8_t i = 0 ; i < COL_SIZE; ++i) {
-        this->OP::Init({KeypadPort_, pins_[i]});
+ for (uint8_t i = 0 ; i < ColSize_; ++i) {
+        this->OP::Init({ColPort_, ColPins_[i]});
     }
 
- for (uint8_t i = COL_SIZE ; i < COL_SIZE + ROW_SIZE; ++i) {
-        this->IP::Init({KeypadPort_, pins_[i]}, Input_mode::kpullup);
+ for (uint8_t i = 0 ; i < RowSize_ ; ++i) {
+        this->IP::Init({RowPort_, RowPins_[i]}, Input_mode::kpullup);
     }
 
  //set column pins to be high (deactivate them)
- for (uint8_t i = 0 ; i < COL_SIZE; ++i) {
-        this->OP::SetHighVoltage({KeypadPort_, pins_[i]});
+ for (uint8_t i = 0 ; i < ColSize_; ++i) {
+        this->OP::SetHighVoltage({ColPort_, ColPins_[i]});
     }
 
 }
+<<<<<<< HEAD
 void Keypad::KeypadKeysInit(char arr[ROW_SIZE][COL_SIZE])
+=======
+void Keypad::KeypadKeysInit(char** arr)
+>>>>>>> e907440 (Updating keypad driver + updating InputDeviceInterface (@Mariamahmed44, @abadr99))
 {
- for (uint8_t i = 0 ; i < COL_SIZE; ++i) {
-   for (uint8_t y=0 ; y < ROW_SIZE; ++y)
+ for (uint8_t i = 0 ; i < ColPort_; ++i) {
+   for (uint8_t y=0 ; y < RowSize_; ++y)
    {
      KEYPAD_KEYS[i][y]=arr[i][y];
    }  
@@ -46,30 +58,30 @@ uint8_t Keypad::KeypadGetPressed()
 
  uint8_t RowCounter,
         ColCounter,
-        Counter=COL_SIZE,
         KeyValue=Keypad::KNotPressed;
  DigitalLevel PinVal;
 
- for (ColCounter=0; ColCounter < COL_SIZE; ColCounter++ )
+ for (ColCounter=0; ColCounter < ColSize_; ColCounter++ )
  {  /*Activate Column by Setting 0 at output*/
-    OP::SetLowVoltage({KeypadPort_, pins_[ColCounter]});
+    OP::SetLowVoltage({ColPort_, ColPins_[ColCounter]});
 
-    for (RowCounter=0; RowCounter < ROW_SIZE ; RowCounter++, Counter++)
+    for (RowCounter=0; RowCounter < RowSize_ ; RowCounter++)
     {  /*get row value*/
-       PinVal= GetPinState({KeypadPort_, pins_[Counter]});
+       PinVal= GetPinState({RowPort_, RowPins_[RowCounter]});
        if (PinVal==DigitalLevel::kLow) //low-> switch is pressed
        {
         /*get the value of the pressed key*/
-       KeyValue=KEYPAD_KEYS[Counter][ColCounter];
+       KeyValue=KEYPAD_KEYS[RowCounter][ColCounter];
        /*wait until its not pressed*/
-       while (GetPinState({KeypadPort_, pins_[Counter]})==DigitalLevel::kLow) 
+       while (GetPinState({RowPort_, RowPins_[RowCounter]})==DigitalLevel::kLow) 
              {/*empty*/}
        /*wait for debouncing time*/
        _delay_ms(KEYPAD_DEBOUNCING_TIME);
        }
        
     }
-   OP::SetHighVoltage({KeypadPort_, pins_[ColCounter]});
+   //De activate the column
+   OP::SetHighVoltage({ColPort_, ColPins_[ColCounter]});
 }
         return KeyValue;
 }
